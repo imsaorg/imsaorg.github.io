0ad17917dee578b730f5c61b443d022b2a4379ae
imsaorg <ahmedkashif0102@gmail.com>
2026-07-10 02:07:12 +0530

    Added the rest of the pages
    Added events, announcements, join us, etc pages. Still WIP.

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,19 @@
-var store = []
+var store = [{
+        "title": "New Years Eve is Awesome!",
+        "excerpt":"Hey There!  ","categories": [],
+        "tags": [],
+        "url": "/announcements/2011/12/new-years-eve-is-awesome",
+        "teaser": null
+      },{
+        "title": "welcome to imsa, here",
+        "excerpt":"Hey there blud!  ","categories": [],
+        "tags": [],
+        "url": "/events/2026/07/welcome-zu-imsa-hehe",
+        "teaser": null
+      },{
+        "title": "KNRUHS is down again",
+        "excerpt":"whens teh website gonna go up, idk  ","categories": [],
+        "tags": [],
+        "url": "/announcements/2026/07/knruhs-is-down-again",
+        "teaser": null
+      }]
