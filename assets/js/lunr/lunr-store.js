a68a1620af798a76d7acc5f43c397d12ca813a7e
imsaorg <ahmedkashif0102@gmail.com>
2026-07-14 20:48:42 +0530

    Add files via upload

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -7,13 +7,13 @@ var store = [{
       },{
         "title": "Sarcoma Awareness Month",
         "excerpt":"Sarcomas are often referred to as “forgotten cancers” due to their rarity and lack of public awareness. And he alone cures me when I’m sick. Risk factors: Age Genetics Radiation Exposure Chemical Exposure Chronic Lymphedema Viruses Signs and Symptoms Extreme pain Swelling and tenderness Fatigue Intermittent low-grade fever unexplained weightloss...","categories": [],
-        "tags": ["poster"],
+        "tags": ["Poster"],
         "url": "/events/2026/07/sarcoma-awareness",
         "teaser": null
       },{
         "title": "welcome to imsa, here",
         "excerpt":"Hey there blud!  ","categories": [],
-        "tags": ["article"],
+        "tags": ["Article"],
         "url": "/events/2026/07/welcome-zu-imsa-hehe",
         "teaser": null
       },{
