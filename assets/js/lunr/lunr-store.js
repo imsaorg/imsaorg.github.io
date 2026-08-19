be9cfec989789ce7536f39738ec182c363f39661
imsaorg <ahmedkashif0102@gmail.com>
2026-08-20 04:42:08 +0530

    First part of new update

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,10 +1,4 @@
 var store = [{
-        "title": "New Years Eve is Awesome!",
-        "excerpt":"Hey There!  ","categories": [],
-        "tags": [],
-        "url": "/new-years-eve-is-awesome/",
-        "teaser": null
-      },{
         "title": "Sarcoma Awareness Month",
         "excerpt":"Sarcomas are often referred to as “forgotten cancers” due to their rarity and lack of public awareness. And he alone cures me when I’m sick. Risk factors: Age Genetics Radiation Exposure Chemical Exposure Chronic Lymphedema Viruses Signs and Symptoms Extreme pain Swelling and tenderness Fatigue Intermittent low-grade fever unexplained weightloss...","categories": [],
         "tags": ["Poster"],
@@ -17,15 +11,21 @@ var store = [{
         "url": "/welcome-zu-imsa-hehe/",
         "teaser": null
       },{
-        "title": "KNRUHS is down again",
-        "excerpt":"whens teh website gonna go up, idk I wonder whats gonna happen next, ayyy  ","categories": [],
-        "tags": [],
-        "url": "/announcements/2026/07/knruhs-is-down-again",
-        "teaser": null
-      },{
         "title": "Starting New Study Circles!",
         "excerpt":"We shall be having daily study sessions of about 1-3 hrs Inshallah. For boys, there shall be pilot venue arrangements of offline venues. For girls, online Zoom groups shall be help. Ebst of luck.  ","categories": [],
         "tags": [],
         "url": "/starting-new-study-circles/",
+        "teaser": null
+      },{
+        "title": "Independance Day",
+        "excerpt":"A day to celebrate 80 years of freedom &amp; our journey towards a healthier India! And remember the sacrifices our ancestors gave for our freedom. Access to healthcare Over 1.38 qualified doctors 209,000+ public health facilities Medical Milestones 1971 : Mahalanabis demonstrated the life saving power of ORS 1978 :...","categories": [],
+        "tags": ["Poster"],
+        "url": "/events/2026/08/independence-day",
+        "teaser": null
+      },{
+        "title": "Final Year MBBS Guidance session for 2022 batch",
+        "excerpt":"A guidance session for final year students will be conducted on August 22 and 23, 2026, spread over two days. Don’t forget to join the Zoom meeting for it!  ","categories": [],
+        "tags": [],
+        "url": "/final-year-guidance/",
         "teaser": null
       }]
